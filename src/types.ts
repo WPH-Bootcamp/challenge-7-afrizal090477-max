a8e28baf65117a8aa1c3029f64ee7881b4bf2f6e
Riza Yakub <afrizal.090477@gmail.com>
2026-05-03 19:58:45 +0700

    feat: mendefinisikan Todo interface dan function types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 // TODO: Definisikan tipe data untuk To-Do item di sini
+
 // Hint: To-Do sebaiknya memiliki id, text, dan status completed
 
 // TODO: Buat interface untuk To-Do item
@@ -6,3 +7,16 @@
 // TODO: Buat tipe untuk status To-Do (active/done)
 
 // TODO: Buat tipe untuk fungsi-fungsi yang akan digunakan
+export interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+export type TodoStatus = "ACTIVE" | "DONE";
+
+export type AddTodoFn = (text: string) => void;
+export type CompletedTodoFn = (id: number) => void;
+export type DeleteTodoFn = (id: number) => void;
+export type ListTodoFn = () => void;
+
